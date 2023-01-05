9da236dceb6177bedc33d7d4c6b226062e14cdac
karlhulme <karlhulme@hotmail.com>
2023-01-05 20:41:33 +0000

    Common errors given support for additional headers.

--- src/responses/badInputErrorResponse.ts
+++ src/responses/badInputErrorResponse.ts
@@ -3,14 +3,19 @@ import { HttpError } from "../index.ts";
 /**
  * Returns a response for when the input has been rejected.
  * @param err An HTTP error.
+ * @param additionalHeaders Additional headers to be appended to the response.
  */
-export function badInputErrorResponse(err: HttpError) {
+export function badInputErrorResponse(
+  err: HttpError,
+  additionalHeaders?: HeadersInit,
+) {
   return new Response(
     `${err.code} ${err.errorCode} ${err.description}\n${err.details}`,
     {
       status: err.code,
       headers: {
         "content-type": "text/plain;charset=utf-8",
+        ...additionalHeaders,
       },
     },
   );
--- src/responses/internalServerErrorResponse.ts
+++ src/responses/internalServerErrorResponse.ts
@@ -1,8 +1,12 @@
 /**
  * Returns a response for when an internal server error occurs.
  * @param underlyingRequest The request being processed.
+ * @param additionalHeaders Additional headers to be appended to the response.
  */
-export function internalServerErrorResponse(underlyingRequest: Request) {
+export function internalServerErrorResponse(
+  underlyingRequest: Request,
+  additionalHeaders?: HeadersInit,
+) {
   const headline =
     "500 INTERNAL_SERVER_ERROR Unexpected error raised processing request.";
   const details = `${underlyingRequest.method} ${underlyingRequest.url}`;
@@ -13,6 +17,7 @@ export function internalServerErrorResponse(underlyingRequest: Request) {
       status: 500,
       headers: {
         "content-type": "text/plain;charset=utf-8",
+        ...additionalHeaders,
       },
     },
   );
