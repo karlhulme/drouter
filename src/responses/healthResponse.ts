35fb0fe0cc340906719a164c9c5db53866860a39
karlhulme <karlhulme@hotmail.com>
2023-01-30 13:19:13 +0000

    Show build gh commit and datetime on health page.

--- src/responses/healthResponse.ts
+++ src/responses/healthResponse.ts
@@ -2,5 +2,10 @@
  * Returns a health response.
  */
 export function healthResponse() {
-  return new Response("Healthy.");
+  const ghCommit = Deno.env.get("BUILD_GH_COMMIT");
+  const dateTime = Deno.env.get("BUILD_DATE_TIME");
+
+  return new Response(
+    `Healthy.\n\nBuild Github Commit: ${ghCommit}\nBuild Date/time: ${dateTime}`,
+  );
 }
